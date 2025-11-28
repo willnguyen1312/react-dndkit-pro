06dc1c97e3acae25192895a7b5618d4b9696399e
Nam Nguyen <nam.nguyen.node@gmail.com>
2025-11-28 14:50:31 -0700

    💅

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,20 +1,14 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+test("test", async ({ page }) => {
+  await page.goto("http://localhost:5173");
+  const button = page.getByRole("button", { name: "Drag me" });
+  const container = page.getByText("Drop here B");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  expect(button).toBeVisible();
+  expect(container).toBeVisible();
 
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+  await button.dragTo(container);
 
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole("heading", { name: "Installation" })
-  ).toBeVisible();
+  await expect(button).toHaveCount(0);
 });
